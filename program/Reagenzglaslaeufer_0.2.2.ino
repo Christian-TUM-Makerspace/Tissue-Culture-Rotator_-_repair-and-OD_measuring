168ff802b581c0e039482604dc7d9b5a7a43d64e
Christian-TUM-Makerspace <80117759+Christian-TUM-Makerspace@users.noreply.github.com>
2021-04-14 11:54:18 +0200

    delete xTask
    Probably xTask is not necessary

--- program/Reagenzglaslaeufer_0.2.2.ino
+++ program/Reagenzglaslaeufer_0.2.2.ino
@@ -3,7 +3,7 @@
 //Tasks to be implemented are listed below the code
 
 // Christian Schormair christian.schormair@tum.de
-// Hans ... Nachname...?
+// Hans Ewigmann
 
 //libraries
 #include <ESP_FlexyStepper.h>
@@ -12,53 +12,26 @@
 const int tubecount = 30;
 int tubes[tubecount]={0};
 
-//Task handles for Multitasking through FreeRTOS (see "Information about multitasking on ESP32" below)
-TaskHandle_t readOptHandle=NULL;
-TaskHandle_t readHallHandle=NULL;
-TaskHandle_t readStopButtonHandle=NULL;
 //ESP-FlexyStepper library already has built in xTaskCreate, without vTaskDelay()
 
 void setup() {
   Serial.begin(115200);
-  
-  
-  //create readOpt task through FreeRTOS, executed in the readOpt() function, priority 3, core 0
-  xTaskCreatePinnedToCore(
-                    readOpt,     // Function that implements the task.
-                    "readOpt",   // name of task.
-  /*reduce when I understand it better */10000,       // Stack size in bytes, not words.
-                    NULL,        // Parameter passed into the task.   or: ( void * ) 1,    
-                    3,           // priority of the task
-                    &readOptHandle,// Task handle to keep track of created task
-                    0);          // pin task to core 0
-  //The same for hall effect sensor and stop button, each: priority lower priority than readOpt, core 1
-  xTaskCreatePinnedToCore(readHall,"readHall", 10000, NULL, 1, &readHallHandle, 1);
-  xTaskCreatePinnedToCore(readStopButton,"readStopButton", 10000, NULL, 2, &readStopButtonHandle, 1);
-  
 }
 
-void readHall(void * pvParameters){
-//  delay(1020);
-  for(;;){ // infinite loop
-    Serial.println("readHall");
 
-    // Pause the task for 500ms
-    vTaskDelay(500 / portTICK_PERIOD_MS);
+void readHall(void){
   }
   
 }
-void readStopButton(void * pvParameters){
-//  delay(1030);
+
+//replace by an interrupt
+void readStopButton(void){
   for(;;){ // infinite loop
-    Serial.println("readStopButton");
-    
-    // Pause the task for 500ms
-    vTaskDelay(500 / portTICK_PERIOD_MS);
   }
 }
 
-void readOpt(void * pvParameters){
-//  delay(1010);
+void readOpt(void){
+  
   for(;;){ // infinite loop
 
     Serial.println("readOpt");
@@ -83,9 +56,10 @@ void loop() {
   //https://docs.espressif.com/projects/esp-idf/en/latest/esp32/api-reference/system/freertos.html#_CPPv412TaskHandle_t
 
 // "at the same time, all the time":
-  //drive stepper motor ESP_FlexyStepper.h  https://github.com/gin66/FastAccelStepper (https://github.com/pkerspe/ESP-FlexyStepper)
+  //drive stepper motor via PWM FastAccelStepper.h  https://github.com/gin66/FastAccelStepper (https://github.com/pkerspe/ESP-FlexyStepper)
   //read opt101
-  //read hall sensor (maybe only when its likely that the magnet is near, between opt101)
+  //read hall sensor (only when its likely that the magnet is near)
+  //read stopSwitch via interrupt
 
 
 
